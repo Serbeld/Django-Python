abc4a15a79858c64a59338989797aa78497389c0
serbeld <serbeldiaz@gmail.com>
2021-09-25 13:19:33 -0500

    Populations users using models on Django

--- Proyectos de Django/Django level two/ProTwo/ProTwo/urls.py
+++ Proyectos de Django/Django level two/ProTwo/ProTwo/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from appTwo import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('users/', include('appTwo.urls'))
 ]
--- Proyectos de Django/Django level two/ProTwo/appTwo/models.py
+++ Proyectos de Django/Django level two/ProTwo/appTwo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    firs_name = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=128)
     last_name = models.CharField(max_length=128)
     email = models.EmailField(max_length=264, unique=True)
     
